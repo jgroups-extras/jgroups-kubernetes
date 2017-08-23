43f5c7275fbd7deea0560d8c58f81b5326631a6f
Sebastian Laskawiec <slaskawi@redhat.com>
2017-08-23 13:12:32 +0200

    Introduced split_clusters_during_rolling_update
    Fixes #33

--- src/test/java/org/jgroups/ping/kube/test/ClientTest.java
+++ src/test/java/org/jgroups/ping/kube/test/ClientTest.java
@@ -1,13 +1,14 @@
 
 package org.jgroups.ping.kube.test;
 
-import org.jgroups.protocols.kubernetes.Client;
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.jgroups.protocols.kubernetes.Client;
+import org.jgroups.protocols.kubernetes.Pod;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
@@ -17,10 +18,10 @@ public class ClientTest {
     @Test
     public void testPods() throws Exception {
         Client client = new TestClient();
-        List<String> pods = client.getPods(null, null, false);
+        List<Pod> pods = client.getPods(null, null, false);
         Assert.assertNotNull(pods);
         assertEquals(2, pods.size());
-        String pod = pods.get(0);
+        String pod = pods.get(0).getIp();
         Assert.assertNotNull(pod);
     }
 
