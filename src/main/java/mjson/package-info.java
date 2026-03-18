3aa9e81cbd471fb83c9593f18361dd09bccf5de6
Radoslav Husar <radosoft@gmail.com>
2026-03-18 11:30:34 +0100

    Replace copied mjson with a standard library that is already provided by runtimes - org.eclipse.parsson:parsson; resolve #288
    Signed-off-by: Radoslav Husar <radosoft@gmail.com>

--- src/main/java/mjson/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The library in this package has been copied directly from https://github.com/bolerio/mjson
- *
- * We want to avoid dependencies.
- */
-package mjson;
--- src/test/java/org/jgroups/ping/kube/test/JsonTest.java
+++ src/test/java/org/jgroups/ping/kube/test/JsonTest.java
@@ -1,10 +1,14 @@
 package org.jgroups.ping.kube.test;
 
-import mjson.Json;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.json.JsonValue;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.List;
+import java.io.StringReader;
 
 import static org.jgroups.protocols.kubernetes.Utils.readFileToString;
 import static org.junit.Assert.assertEquals;
@@ -19,26 +23,20 @@ public class JsonTest {
     public void testJsonParser() throws Exception {
         String input=readFileToString(new File(TestClient.class.getResource("/pods_without_ports.json").toURI()));
 
-        Json json=Json.read(input);
-
-        List<Json> items=json.at("items").asJsonList();
-
-        for(Json obj: items) {
-            Json status=obj.at("status");
-            Json phase=status.at("phase");
-            Json ip=status.at("podIP");
-            System.out.printf("%s -> %s\n", ip, phase);
-            assertEquals("Running", phase.asString());
+        JsonObject json;
+        try (JsonReader reader = Json.createReader(new StringReader(input))) {
+            json = reader.readObject();
         }
 
-        /* JsonObject root=JsonParser.object().from(input);
-        JsonArray items=root.getArray("items");
+        JsonArray items=json.getJsonArray("items");
 
-        for(int i=0; i < items.size(); i++) {
-            JsonObject obj=items.getObject(i);
-            JsonObject status=obj.getObject("status");
-            String hostIP=status.getString("hostIP");
-            System.out.println("hostIP = " + hostIP);
-        }*/
+        for(JsonValue item: items) {
+            JsonObject obj=item.asJsonObject();
+            JsonObject status=obj.getJsonObject("status");
+            String phase=status.getString("phase");
+            String ip=status.getString("podIP");
+            System.out.printf("%s -> %s\n", ip, phase);
+            assertEquals("Running", phase);
+        }
     }
 }
